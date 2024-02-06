0ef8dd6668ad3b6013593321d0df1cef4b7f0e3d
Ryuda <ryuda@nate.com>
2024-02-07 00:54:16 +0900

    Init Files

---
+++ src/main/java/com/springbootquartz/SpringbootQuartzApplication.java
@@ -0,0 +1,15 @@
+package com.springbootquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringbootQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQuartzApplication.class, args);
+    }
+
+}
